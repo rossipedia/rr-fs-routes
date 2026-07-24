049f30feed2a77e714ae02bd9d059e7d6f77cb1e
Bryan Ross <git@rossipedia.com>
2026-07-24 10:10:49 -0600

    just pnpm it

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite-plus'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  fmt: {
-    semi: false,
-    arrowParens: 'always',
-    insertFinalNewline: true,
-    printWidth: 80,
-    singleQuote: true,
-    sortImports: true,
-    sortPackageJson: true,
-    sortTailwindcss: true,
-  },
-  lint: {
-    jsPlugins: [{ name: 'vite-plus', specifier: 'vite-plus/oxlint-plugin' }],
-    rules: { 'vite-plus/prefer-vite-plus-imports': 'error' },
-    options: { typeAware: true, typeCheck: true },
-  },
   plugins: [tailwindcss(), reactRouter()],
 })
